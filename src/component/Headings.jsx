b8a586206c069ceac4e45a68cbcc5eeabcfc3fa6
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-10 07:36:59 +0530

    import react and create Headings function return jsx for Top Heading.

---
+++ src/component/Headings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Headings = () => {
+  return (
+    <div className='Heading'>
+      <h1>Bank Accounts Registration Form</h1>
+    </div>
+  );
+};
+
+export default Headings;
