9c84ae1587c1ca04b179a92642c3ab7ea5d11287
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-10 07:53:14 +0530

    importing Heading and InputFormMain Component and inside App function return these component.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Headings from './component/Headings';
+import InputFormMain from './component/MainState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Headings />
+      <InputFormMain />
+    </>
   );
 }
 
